fb720b05b1200ad0b4aeb041f5ae9e79d8ae5c7f
maryjoylimjoco <maryjoylimjoco@gmail.com>
2019-01-04 15:18:15 +0800

    fixes routes.rb conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  api_version(:module => "Api::V1", :header => {:name => "Accept", :value => "application/vnd.gameworks.io; version=1"}, :parameter => {:name => "version", :value => "1"}, :path => {:value => "v1"}, :defaults => {:format => "json"}, :default => true) do
+  api_version(:module => "Api::V1", :header => {
+    :name => "Accept",
+    :value => "application/vnd.gameworks.io; version=1"},
+    :parameter => {:name => "version", :value => "1"},
+    :path => {:value => "v1"}, :defaults => {:format => "json"},
+    :default => true) do
+
     resources :users
     resources :players
     resources :publishers
