0d5961c51265e443cd29cdc77ff0207fdcb53098
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-01-08 11:58:49 +0800

    Reconstructed the User, Publisher, and Player routes. Remove the 'delete' action.

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,22 @@ Rails.application.routes.draw do
     :path => {:value => "v1"}, :defaults => {:format => "json"},
     :default => true) do
 
-    resources :users
-    resources :players
-    resources :publishers
+    resources :users, :except => [:destroy]
+
+    resources :publishers, :except => [:show, :update, :destroy] do
+      collection do
+        get   "/:user_id", to: "publishers#show"
+        patch "/:user_id", to: "publishers#update"
+        put   "/:user_id", to: "publishers#update"
+      end
+    end
+    
+    resources :players, :except => [:show, :update, :destroy] do
+      collection do
+        get   "/:user_id", to: "players#show"
+        patch "/:user_id", to: "players#update"
+        put   "/:user_id", to: "players#update"
+      end
+    end
   end
 end
