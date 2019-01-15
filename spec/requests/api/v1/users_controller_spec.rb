45fc1663a7f2eadf5588c56b5a4165a490a7dc35
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-01-15 14:12:51 +0800

    Added rspec test for testing the User endpoint GET /api/v1/users

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -2,4 +2,17 @@ require "rails_helper"
 
 describe Api::V1::UsersController do
 
+  it "should implement the endpoint GET /api/v1/users" do
+    
+    # setup 5 sample Users
+    5.times do
+      user = create(:user)
+    end
+    
+    # call the API endpoint
+    get "/api/v1/users"
+    
+    # make sure login was successful
+    expect(response).to have_http_status(:ok)
+  end  
 end
