0624761b294f01adcbae38a6250db1e7a539466f
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-01-15 14:03:52 +0800

    Added password as part of the attribute for the User mock model.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name  { Faker::Name.last_name  }
     email      { "#{first_name.downcase}.#{last_name.downcase}@example.com" }
+    password   { email                  }
   end
 
 end
