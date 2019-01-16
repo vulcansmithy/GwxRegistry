b11a44c711528077ffc2cb830f2c338e49dec694
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-01-16 16:48:59 +0800

    Enable rspec for GET /v1/users endpoint. Additional minor code refactor.

--- spec/requests/api/v1/users_controller_spec.rb
+++ spec/requests/api/v1/users_controller_spec.rb
@@ -2,10 +2,13 @@ require "rails_helper"
 
 describe Api::V1::UsersController do
 
-  xit "should implement the endpoint GET /users" do
+  it "should implement the endpoint GET /users" do
+    
+    # set the no. of Users accounts
+    no_of_users = 5
     
     # setup 5 sample Users
-    5.times do
+    no_of_users.times do
       user = create(:user)
     end
     
@@ -14,6 +17,9 @@ describe Api::V1::UsersController do
     
     # make sure login was successful
     expect(response).to have_http_status(:ok)
+    
+    # make sure the no_of_users matches
+    expect(JSON.parse(response.body)["users"].length).to eq no_of_users
   end 
   
   it "should implement the endpoint POST /users" do
