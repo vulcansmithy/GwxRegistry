9008a9be42b8204be24288b50247b34b7741c6b3
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-01-16 16:51:29 +0800

    Added a helper method for successful response and for error response.

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::BaseController < ApplicationController
+
+  def success_response(response_payload, status_code=:ok)
+    render json: response_payload, status: status_code
+  end
+  
+  def error_response(message, status_code)    
+    render json: { message: message }, status: status_code
+  end
+  
 end
