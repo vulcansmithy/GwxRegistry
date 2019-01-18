44b8ed597ca4dbebe8cb724af6c2ea79d7374c01
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-01-18 16:23:29 +0800

    Added routes for PUTS /users/profile_update/:id.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     resources :users, :except => [:destroy] do
       collection do
         patch "/profile_update/:id", to: "users#profile_update"
+        put   "/profile_update/:id", to: "users#profile_update"
         patch "/account_update/:id", to: "users#account_update"
         
         post "/register",            to: "users#register"
