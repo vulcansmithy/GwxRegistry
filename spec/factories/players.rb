7adc2164832aa4475d1ff513d80bd88b72fa9448
maryjoylimjoco <maryjoylimjoco@gmail.com>
2019-01-18 15:21:21 +0800

    modifies json response

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
 
   factory :player do
-    username { Faker::Name.unique.name }
-    wallet_address { Faker::Number.number(20) }
+    username        { Faker::Name.unique.name }
+    wallet_address  { Faker::Number.number(20) }
   end
-
 end
