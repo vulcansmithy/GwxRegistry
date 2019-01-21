9e71e28f566ae410916a435f13afc1078f50d276
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-01-21 11:46:33 +0800

    Minor code cleanup.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
   has_one :player, dependent: :destroy
   has_one :publisher, dependent: :destroy
 
-  validates_presence_of :email, :password
+  validates_presence_of   :email, 
+  validate_presence_of    :password
   validates_uniqueness_of :email
-  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-  validates_length_of :password, minimum: 8
+  validates_format_of     :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates_length_of     :password, minimum: 8
 end
