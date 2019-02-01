fb14b2698090980e9aca52af84c9e2291e181975
Nico Bantatua <nico@medifi.com>
2019-02-01 17:32:57 +0800

    Fixes commented code

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,16 +49,16 @@ namespace :puma do
 end
 
 namespace :deploy do
-  # desc "Make sure local git is in sync with remote."
-  # task :check_revision do
-  #   on roles(:app) do
-  #     unless `git rev-parse HEAD` == `git rev-parse origin/master`
-  #       puts "WARNING: HEAD is not the same as origin/master"
-  #       puts "Run `git push` to sync changes."
-  #       exit
-  #     end
-  #   end
-  # end
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    # on roles(:app) do
+    #   unless `git rev-parse HEAD` == `git rev-parse origin/master`
+    #     puts "WARNING: HEAD is not the same as origin/master"
+    #     puts "Run `git push` to sync changes."
+    #     exit
+    #   end
+    # end
+  end
 
   desc 'Initial Deploy'
   task :initial do
