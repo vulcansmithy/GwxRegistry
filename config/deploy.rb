6ea2ee7f3ff7ca6dabaf404680394165c37051c1
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-02-21 17:35:02 +0800

    Reverts deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,6 @@ set :puma_workers,    0
 
 set :pty,             true
 set :use_sudo,        false
-set :stages,          %i(staging production)
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
