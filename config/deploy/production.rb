65f7ea0e0a05bf087d0b43fa3e2b4b9a7495c95e
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-02-21 17:37:20 +0800

    Removes set env in env files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,3 @@
 server "13.229.58.101", user: "ubuntu", roles: %w{app web}
-set :stage, :production
 set :rails_env, :production
 set :branch, 'master'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,3 @@
 server "13.229.137.10", user: "ubuntu", roles: %w{app web}
-set :stage, :staging
 set :rails_env, :staging
 set :branch, 'develop'
