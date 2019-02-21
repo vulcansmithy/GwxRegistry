788ee7811c4613003337e680bfc1925617d7721c
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-02-21 15:57:46 +0800

    Sets stage to respective env files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,3 @@
 server "13.229.58.101", user: "ubuntu", roles: %w{app web}
+set :stage, :production
 set :branch, 'master'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,3 @@
 server "13.229.137.10", user: "ubuntu", roles: %w{app web}
+set :stage, :staging
 set :branch, 'develop'
