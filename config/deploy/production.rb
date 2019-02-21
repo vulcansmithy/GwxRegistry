f0da246fe37b55232ca1d02602eef3742260fe5d
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-02-21 16:02:18 +0800

    Sets rails env in respective env files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 server "13.229.58.101", user: "ubuntu", roles: %w{app web}
 set :stage, :production
+set :rails_env, :production
 set :branch, 'master'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,4 @@
 server "13.229.137.10", user: "ubuntu", roles: %w{app web}
 set :stage, :staging
+set :rails_env, :staging
 set :branch, 'develop'
