6baa4256e32b07d1bffc5d5f86198324b492353c
maryjoylimjoco <maryjoylimjoco@gmail.com>
2019-02-28 08:22:03 +0800

    removes index endpoint and added an endpoint in publisher

--- spec/integration/registry_api_spec.rb
+++ spec/integration/registry_api_spec.rb
@@ -1,6 +1,7 @@
 require "swagger_helper"
 
 describe "Gameworks Registry API" do
+=begin
   ## Users
   # POST /login
   path "/login" do
@@ -42,85 +43,6 @@ describe "Gameworks Registry API" do
         run_test!
 
         response "401", "Invalid Credentials" do
-          run_test!
-        end
-      end
-    end
-  end
-
-  # GET /users
-  path "/users" do
-    get "Retrieve all User accounts" do
-      tags        "Users"
-      description "Retrieve all User accounts."
-      produces    "application/json"
-      parameter name: :authorization, in: :header, description: "token provided to user upon log in", required: true, type: :string
-
-      response "200", "user(s) found." do
-
-        examples "application/json" => {
-          "data" => [
-            {
-                      "id" => "17",
-                    "type" => "user",
-              "attributes" => {
-                              "id" => 17,
-                      "first_name" => "Duane",
-                       "last_name" => "Kohler",
-                           "email" => "duane.kohler@example.com",
-                  "wallet_address" => "b6a86d69ff07758f3ee4a894fce8849309af74179695e3ccfb314db489a94dc8"
-              }
-            },
-            {
-                      "id" => "18",
-                    "type" => "user",
-              "attributes" => {
-                              "id" => 18,
-                      "first_name" => "Shin",
-                       "last_name" => "McDermott",
-                           "email" => "shin.mcdermott@example.com",
-                  "wallet_address" => "f588eaf1048bc74a8c69e20e751b42d0eda50045cfd12594d363aac289ff8e78"
-              }
-            },
-            {
-                      "id" => "19",
-                    "type" => "user",
-              "attributes" => {
-                              "id" => 19,
-                      "first_name" => "Williams",
-                       "last_name" => "Gaylord",
-                           "email" => "williams.gaylord@example.com",
-                  "wallet_address" => "92ce8f0147db4be1ae52858a530daf1f05ce8a611eef9fa5de4f6ba0d4d5f118"
-              }
-            },
-            {
-                      "id" => "20",
-                    "type" => "user",
-              "attributes" => {
-                              "id" => 20,
-                      "first_name" => "Maple",
-                       "last_name" => "Schuppe",
-                           "email" => "maple.schuppe@example.com",
-                  "wallet_address" => "ceef5f3f9d7066ce2b5fec14559164c997a6aec2f7f6b4e8aab72d30211cf11d"
-              }
-            },
-            {
-                      "id" => "21",
-                    "type" => "user",
-              "attributes" => {
-                              "id" => 21,
-                      "first_name" => "Carylon",
-                       "last_name" => "Russel",
-                           "email" => "carylon.russel@example.com",
-                  "wallet_address" => "71037afb5e123b65e2fc2a0666826df3bc6611b39792d14be629c9620c392a57"
-              }
-            }
-          ]
-        }
-
-        run_test!
-
-        response "401", "Unauthorized: Access is denied" do
           run_test!
         end
       end
@@ -366,84 +288,6 @@ describe "Gameworks Registry API" do
   end
 
   ## Publishers
-  path "/publishers" do
-
-   get "Retrieve all Publisher accounts" do
-    tags        "Publishers"
-    description "Retrieve all publisher accounts."
-    produces    "application/json"
-    parameter   name: :authorization, in: :header, description: "token provided to user upon log in", required: true, type: :string
-
-    response "200", "publisher(s) found." do
-
-      examples "application/json" => {
-        "data" => [
-                    {
-                                "id" => "16",
-                              "type" => "publisher",
-                        "attributes" => {
-                                        "id" => 16,
-                            "publisher_name" => "Clemente Rosenbaum",
-                               "description" => "Labore atque cumque repudiandae.",
-                            "wallet_address" => "3ea0329ca47cfaddffc470637543dfa365ad3efa8a8eb69d82b0d1d994e1fc7c",
-                                   "user_id" => 17
-                      }
-                    },
-                    {
-                              "id" => "17",
-                            "type" => "publisher",
-                      "attributes" => {
-                                      "id" => 17,
-                          "publisher_name" => "Janean Sporer",
-                             "description" => "Eaque ut doloremque sit.",
-                          "wallet_address" => "65622dfe102a4ee9a30584008b1130814d525521d96f355824b9a02791e12c89",
-                                 "user_id" => 18
-                        }
-                    },
-                    {
-                                "id" => "18",
-                              "type" => "publisher",
-                        "attributes" => {
-                                        "id" => 18,
-                            "publisher_name" => "Kent Monahan II",
-                               "description" => "Autem aut mollitia eum.",
-                            "wallet_address" => "6c74a6311f38eadd4302e12869eccc9e2363aed4955156c735cb687e58c2d42e",
-                                   "user_id" => 19
-                        }
-                    },
-                    {
-                                "id" => "19",
-                              "type" => "publisher",
-                        "attributes" => {
-                                        "id" => 19,
-                            "publisher_name" => "Thurman Maggio",
-                               "description" => "Repellendus sit magni nisi.",
-                            "wallet_address" => "c1f740400f0a90dec1cd99dcc25b0c27c3cbb15eb85e54a1d344f7af3aab4779",
-                                   "user_id" => 20
-                        }
-                    },
-                    {
-                              "id" => "20",
-                            "type" => "publisher",
-                      "attributes" => {
-                                      "id" => 20,
-                          "publisher_name" => "Sidney Murray",
-                             "description" => "Iste aut praesentium nostrum.",
-                          "wallet_address" => "fa4383f66b93aa438b91a2cd82e402797b67c07c4ef0b5b38c954b5231dcbcdf",
-                                 "user_id" => 21
-                    }
-                }
-            ]
-      }
-
-      run_test!
-
-      response "401", "Unauthorized: Access is denied" do
-        run_test!
-      end
-    end
-  end
-end
 
   path "/publishers/{user_id}" do
 
@@ -577,94 +421,60 @@ end
     end
   end
 
+  # PUT /publishers/:user_id
+  path "/publishers/{user_id}" do
 
-  ## Players
-  # GET /players
-  path "/players" do
+    put "Update Publisher account" do
+      tags        "Publishers"
+      description "Update an existing Publisher account."
+      consumes    "application/json", "application/xml"
+      parameter   name: :user_id,   in: :path, description: "'id' of the User profile being retrieved", required: true, type: :integer
+      parameter   name: :publisher, in: :body, schema: {
+        type: :object,
+        properties: {
+          publisher_name: { type: :string },
+          wallet_address: { type: :string },
+          description:    { type: :string }
+        }
+      }
+      parameter name: :authorization, in: :header, description: "token provided to user upon log in", required: true, type: :string
 
-    get "Retrieve all Player profiles" do
-      tags        "Players"
-      description "Retrieve all existing Player profiles."
-      produces    "application/json"
-      parameter   name: :authorization, in: :header, description: "token provided to user upon log in", required: true, type: :string
-
-      response "200", "player(s) found." do
+      response "200", "publisher updated." do
 
         examples "application/json" => {
-            "data" => [
-                    {
-                            "id" => "621",
-                          "type" => "player",
-                    "attributes" => {
-                               "user_id" => 1259,
-                            "first_name" => "Michaela",
-                             "last_name" => "Bruen",
-                                 "email" => "michaela.bruen@example.com",
-                              "username" => "michaela.bruen",
-                        "wallet_address" => "6321a571ee1a69c8c44e72b8a4b67c74259024eb18728575984f624923682f85"
-                    }
-                },
-                    {
-                            "id" => "622",
-                          "type" => "player",
-                    "attributes" => {
-                               "user_id" => 1260,
-                            "first_name" => "Clare",
-                             "last_name" => "Dietrich",
-                                 "email" => "clare.dietrich@example.com",
-                              "username" => "clare.dietrich",
-                        "wallet_address" => "a73203391935fda8548debd8679c6bf282e634adf864f77fa0d977fa52f834a2"
-                    }
-                },
-                    {
-                            "id" => "623",
-                          "type" => "player",
-                    "attributes" => {
-                               "user_id" => 1261,
-                            "first_name" => "Cameron",
-                             "last_name" => "Hermiston",
-                                 "email" => "cameron.hermiston@example.com",
-                              "username" => "cameron.hermiston",
-                        "wallet_address" => "bbe29af33171a6cea839817589418de814e9e825dd71fcc810236d18cfeb8f06"
-                    }
-                },
-                    {
-                            "id" => "624",
-                          "type" => "player",
-                    "attributes" => {
-                               "user_id" => 1262,
-                            "first_name" => "Paz",
-                             "last_name" => "Emard",
-                                 "email" => "paz.emard@example.com",
-                              "username" => "paz.emard",
-                        "wallet_address" => "e149817318bb8c3b6fe5c9cfcc55629b006bfdc2c5796595b5e264f7272d5b32"
-                    }
-                },
-                    {
-                            "id" => "625",
-                          "type" => "player",
-                    "attributes" => {
-                               "user_id" => 1263,
-                            "first_name" => "Dian",
-                             "last_name" => "Kuhn",
-                                 "email" => "dian.kuhn@example.com",
-                              "username" => "dian.kuhn",
-                        "wallet_address" => "796e7296f80960683b10d8bc8ac3b8974fb6788a40f9073ed7c5163223280b0b"
-                    }
-                }
-            ]
+          "data" => {
+                      "id" => "54",
+                    "type" => "publisher",
+              "attributes" => {
+                              "id" => 54,
+                  "publisher_name" => "Testing01",
+                     "description" => "Ducimus quisquam ipsam inventore.",
+                  "wallet_address" => "f64789142671fec9435dbe5847bd334a4baed571b1ac647d82fda152e2b645f6",
+                         "user_id" => 55
+              }
+          }
         }
 
-        # @TODO implement the schema
 
         run_test!
+      end
 
-        response "401", "Unauthorized: Access is denied" do
-          run_test!
-        end
+      response "404", "Publisher not found." do
+        run_test!
+      end
+
+      response "422", "Unable to update publisher account." do
+        run_test!
+      end
+
+      response "401", "Unauthorized: Access is denied" do
+        run_test!
       end
     end
   end
+
+
+  ## Players
 
   # GET /players/:user_id
   path "/players/{user_id}" do
@@ -852,4 +662,5 @@ end
       end
     end
   end
+=end
 end
