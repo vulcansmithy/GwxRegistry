8c6e94a69c97be4d9193b1e9b9214ef15d1b7007
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-03-12 11:05:33 +0800

    Add device_token field for Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_084629) do
+ActiveRecord::Schema.define(version: 2019_03_12_020239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_02_27_084629) do
     t.string "encrypted_pk"
     t.string "encrypted_pk_iv"
     t.string "mac_address"
+    t.string "device_token"
     t.index ["email", "mac_address", "confirmation_code"], name: "index_users_on_email_and_mac_address_and_confirmation_code", unique: true
     t.index ["encrypted_pk_iv"], name: "index_users_on_encrypted_pk_iv", unique: true
   end
