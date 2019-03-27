1f85d1a7dbe434305714be914425a9652e7ccbde
Ly Escorial <lyescorial@gmail.com>
2019-03-27 14:59:48 +0800

    Creates wallet generation

--- app/services/nem_service.rb
+++ app/services/nem_service.rb
@@ -1,9 +1,47 @@
 require 'nem'
+require 'securerandom'
+# require_relative './ed25519.rb'
+require 'base32'
 
 class NemService
   def self.generate_account
-    @node = Nem::Node.new(url: 'http://127.0.0.1:7890')
-    @endpoint = Nem::Endpoint::Account.new(@node)
-    @account = @endpoint.generate
+    private_key = SecureRandom.random_bytes(32)
+    pk = ED25519.publickey(private_key.reverse)
+    hex_pk = to_hex(pk)
+  end
+
+  def to_hex(data)
+    [data].unpack('H*').first
+  end
+
+  private
+
+  def to_bin(data)
+    [data].pack('H*')
+  end
+
+  def hash_rmd
+    rmd_pk = Digest::RMD160.hexdigest(sha3_pk)
+  end
+
+  def mainnet
+    versioned_pk = "98" + rmd_pk
+  end
+
+  def testnet
+    versioned_pk = "68" + rmd_pk
+  end
+
+  def hash_sha3
+    checksum = Digest::SHA3.hexdigest(to_bin(versioned_pk),256)[0..7]
+  end
+
+  def concat_result
+    bin_address = to_bin(versioned_pk + checksum)
+  end
+
+  def encode_base32
+    Base32.encode bin_address
+>>>>>>> Creates wallet generation
   end
 end
