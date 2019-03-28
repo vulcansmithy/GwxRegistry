b343d89e533d6775d9fb2c816e8f376b90854d72
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-03-28 18:22:35 +0800

    Modify the serializer to just return the encrypted_pk attribute.

--- app/serializers/wallet_serializer.rb
+++ app/serializers/wallet_serializer.rb
@@ -6,7 +6,10 @@ class WalletSerializer < ActiveModel::Serializer
              :nonce
   
   attribute :encrypted_pk do |wallet|
+    # re-encrypt the pk
     encrypted_payload_base64, @nonce_base64 = SymmetricEncryptionService.new.encrypt(wallet.pk)
+    
+    encrypted_payload_base64
   end 
   
   attribute :nonce do |wallet|
