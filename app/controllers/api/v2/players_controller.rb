5a049384b061878e19a67e2cedee1ab6f263fb28
Jestoni Talan <jestonivtalan@gmail.com>
2019-04-01 18:15:43 +0800

    Adding v2 api

---
+++ app/controllers/api/v2/players_controller.rb
@@ -0,0 +1,60 @@
+class Api::V2::PlayersController < Api::V2::BaseController
+  before_action :transform_params, only: :create
+  before_action :check_current_user, except: %i[index show create my_player]
+  before_action only: :create do
+    check_player_publisher_account(@current_user, "player")
+  end
+  before_action :set_player, only: %i[show edit update]
+
+  def index
+    @players = Player.all
+    success_response(PlayerSerializer.new(@players).serialized_json)
+  end
+
+  def show
+    @player = Player.find(params[:user_id])
+    success_response(PlayerSerializer.new(@player).serialized_json)
+  end
+
+  def my_player
+    success_response(PlayerSerializer.new(@current_user.player).serialized_json)
+  end
+
+  def create
+    @player = @current_user.create_player(player_params)
+
+    if @player.save
+      success_response(PlayerSerializer.new(@player).serialized_json, :created)
+    else
+      error_response("Unable to create player account",
+                     @player.errors.full_messages, :unprocessable_entity)
+    end
+  end
+
+  def edit
+    render json: { player: PlayerSerializer.new(@player).serialized_json }
+  end
+
+  def update
+    if @player.update(player_params)
+      success_response(PlayerSerializer.new(@player).serialized_json)
+    else
+      error_response("Unable to update player account",
+                     @player.errors.full_messages, :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def player_params
+    params.permit(:user_id, :username)
+  end
+
+  def set_player
+    @player = @current_user.player
+    unless @player
+      error_response("You don't have an existing player account",
+                     "Player account does not exist", :not_found)
+    end
+  end
+end
---
+++ app/controllers/api/v2/publishers_controller.rb
@@ -0,0 +1,56 @@
+class Api::V2::PublishersController < Api::V2::BaseController
+  before_action :transform_params, only: %i[create edit update]
+  before_action :check_current_user, except: %i[index show create]
+  before_action only: :create do
+    check_player_publisher_account(@current_user, "publisher")
+  end
+  before_action :set_publisher, only: %i[edit update show]
+
+  def index
+    @publishers = Publisher.all
+    success_response(PublisherSerializer.new(@publishers).serialized_json)
+  end
+
+  def show
+    success_response(PublisherSerializer.new(@publisher).serialized_json)
+  end
+
+  def create
+    @publisher = @current_user.create_publisher(publisher_params)
+
+    if @publisher.save
+      success_response(PublisherSerializer.new(@publisher).serialized_json,
+                       :created)
+    else
+      error_response("Unable to create publisher account",
+                     @publisher.errors.full_messages, :unprocessable_entity)
+    end
+  end
+
+  def edit
+    success_response(PublisherSerializer.new(@publisher).serialized_json)
+  end
+
+  def update
+    if @publisher.update(publisher_params)
+      success_response(PublisherSerializer.new(@publisher).serialized_json)
+    else
+      error_response("Unable to update publisher account",
+                     @current_user.errors.full_messages, :unprocessable_entity)
+    end
+  end
+
+  private
+
+  def publisher_params
+    params.permit(:description, :wallet_address, :user_id, :publisher_name)
+  end
+
+  def set_publisher
+    @publisher = @current_user.publisher
+    unless @publisher
+      error_response("You don't have an existing publisher account",
+                     "Publisher account does not exist", :unprocessable_entity)
+    end
+  end
+end
