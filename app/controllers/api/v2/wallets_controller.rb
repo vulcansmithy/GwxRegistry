598115c0bba22058846d7dd38232c6e36af27baf
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-04-05 20:19:20 +0800

    Debug wallets controller

--- app/controllers/api/v2/wallets_controller.rb
+++ app/controllers/api/v2/wallets_controller.rb
@@ -12,7 +12,10 @@ class Api::V2::WalletsController < Api::V2::BaseController
     @bal = NemService.check_balance(params[:wallet_address])
     wallet = Wallet.find_by(wallet_address: params[:wallet_address])
 
+    puts ">>>>>>>> Wallet Account Type: #{wallet.account_type}"
+    puts ">>>>>>>> Game Wallet Address: #{params[:game_wallet_address]}"
     if wallet.account_type == 'Player' && params[:game_wallet_address]
+      puts ">>>>> Wallet Account is player and game wallet address params is present"
       player_wallet = wallet.wallet_address
       user_wallet = wallet.account.user.wallet.wallet_address
       game_wallet = params[:game_wallet_address]
@@ -37,7 +40,7 @@ class Api::V2::WalletsController < Api::V2::BaseController
 
       current_gwx_balance = @bal[:gwx] || 0 + unconfirmed_rewards + unconfirmed_cashin - unconfirmed_bets
       available_gwx = @bal[:gwx] || 0
-      
+
       response = {
         unconfirmed_bets: unconfirmed_bets,
         unconfirmed_rewards: unconfirmed_rewards,
@@ -46,6 +49,7 @@ class Api::V2::WalletsController < Api::V2::BaseController
         available_gwx: available_gwx
       }.merge(@bal)
 
+      puts ">>>>>>>>>>>>> Response: #{response}"
       success_response({ balance: response })
     else
       success_response({balance: @bal})
