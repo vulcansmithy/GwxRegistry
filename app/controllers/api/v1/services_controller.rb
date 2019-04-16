630730db8c3f83b0417cc97b9bfd939d22149998
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-04-16 17:05:27 +0800

    Updated the public_key endpoint.

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -3,11 +3,7 @@ class Api::V1::ServicesController <  Api::V1::BaseController
   skip_before_action :authenticate_request, only: %i[public_key]
   
   def public_key
-    decoded_private_key = Base64.decode64(Rails.application.secrets.registry_private_key)
-    private_key         = RbNaCl::PrivateKey.new(decoded_private_key)
-    public_key          = Base64.encode64(private_key.public_key)
-
-    success_response({ public_key: public_key })
+    success_response({ public_key: PublicKeyEncryptionService.new.public_key })
   end
 
 end
