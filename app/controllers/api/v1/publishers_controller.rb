9bb51fcf298c8a2afaf6be3024088bfae0af734e
maryjoylimjoco <maryjoylimjoco@gmail.com>
2019-04-25 12:56:52 +0800

    update publisher controller

--- app/controllers/api/v1/publishers_controller.rb
+++ app/controllers/api/v1/publishers_controller.rb
@@ -1,10 +1,5 @@
 class Api::V1::PublishersController < Api::V1::BaseController
   before_action :transform_params, only: %i[create edit update]
-  before_action :check_current_user, except: %i[index show create]
-  before_action only: :create do
-    check_player_publisher_account(@current_user, "publisher")
-  end
-  before_action :set_publisher, only: %i[edit update show]
 
   def index
     @publishers = Publisher.all
