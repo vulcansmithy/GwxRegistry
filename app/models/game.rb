4a1b350065a22059ebcaeca22065952b0d2abc05
Ly Escorial <lyescorial@gmail.com>
2019-05-14 16:41:18 +0800

    Adds after_commit hook to create game wallet

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,16 @@
 class Game < ApplicationRecord
+  after_commit :create_account, on: :create
   belongs_to  :publisher
+  has_one :wallet, as: :account
+  
+  private
+
+  def create_account
+    account = NemService.create_account
+
+    self.create_wallet(
+      wallet_address: account[:address],
+      pk: account[:priv_key]
+    )
+  end
 end
