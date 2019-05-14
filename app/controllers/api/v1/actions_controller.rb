2b48f45c00894244be0725bb9393578f4b2e16b3
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-05-14 16:15:24 +0800

    Add ActionsController

---
+++ app/controllers/api/v1/actions_controller.rb
@@ -0,0 +1,70 @@
+class Api::V1::ActionsController < Api::V1::BaseController
+  before_action :transform_params
+  before_action :set_game
+  before_action :set_action, only: %i[show update destroy]
+
+  def index
+    actions = @game.actions
+    success_response ActionSerializer.new(actions).serialized_json
+  end
+
+  def show
+    action = @game.actions.find params[:id]
+    success_response ActionSerializer.new(action).serialized_json
+  end
+
+  def create
+    action = @game.actions.new action_params
+
+    if action.save
+      success_response ActionSerializer.new(action).serialized_json
+    else
+      error_response(
+        "Unable to create action",
+        action.errors.full_messages,
+        :unprocessable_entity
+      )
+    end
+  end
+
+  def update
+    if @action.update action_params
+      success_response(ActionSerializer.new(action).serialized_json)
+    else
+    end
+  end
+
+  def destroy
+    if @action.destroy
+      render status: :no_content
+    else
+      error_response(
+        "Unable to create action",
+        @action.errors.full_messages,
+        :unprocessable_entity
+      )
+    end
+  end
+
+  private
+
+  def action_params
+    params.permit(
+      :game_id,
+      :name,
+      :description,
+      :fixed_amount,
+      :unit_fee,
+      :fixed,
+      :rate
+    )
+  end
+
+  def set_game
+    @game = Game.find params[:game_id]
+  end
+
+  def set_action
+    @action = @game.actions.find params[:id]
+  end
+end
