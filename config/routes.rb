3e9095cce7e1f3be20589fdffdf1db10b9a12559
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-05-16 14:39:38 +0800

    Fixes routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,6 @@ Rails.application.routes.draw do
 
     get  'public_key', to: 'services#public_key'
     get  'test',       to: 'users#test'
-    post 'login',      to: 'users#login'
-    post 'register',   to: 'users#create'
-    get  'user',       to: 'users#show'
-    post 'forgot',     to: 'users#forgot'
-    post 'notify',     to: 'users#send_notification'
     get  'player',     to: 'player_profiles#my_player'
     get  'publisher',  to: 'publishers#show'
 
@@ -39,8 +34,6 @@ Rails.application.routes.draw do
 
     resources :users, :except => [:destroy, :show] do
       collection do
-        get '/confirm/:code',   to: 'users#confirm'
-        get '/:id/resend_code', to: 'users#resend_code'
         get '/:wallet_address', to: 'users#find_player'
       end
     end
@@ -71,52 +64,52 @@ Rails.application.routes.draw do
   end
   root :to => 'home#index'
 
-  # api_version(
-  #   :module    => "Api::V2",
-  #   :header    => { :name   => "Accept",  :value => "application/vnd.gameworks.io; version=2" },
-  #   :parameter => { :name   => "version", :value => "2" },
-  #   :path      => { :value  => "v2"   },
-  #   :defaults  => { :format => "json" }) do
+  api_version(
+    :module    => "Api::V2",
+    :header    => { :name   => "Accept",  :value => "application/vnd.gameworks.io; version=2" },
+    :parameter => { :name   => "version", :value => "2" },
+    :path      => { :value  => "v2"   },
+    :defaults  => { :format => "json" }) do
 
-  #   get  'public_key', to: 'services#public_key'
-  #   get  'test',       to: 'users#test'
-  #   post 'login',      to: 'users#login'
-  #   post 'register',   to: 'users#create'
-  #   get  'user',       to: 'users#show'
-  #   post 'notify',     to: 'users#send_notification'
-  #   get  'player',     to: 'players#my_player'
-  #   get  'publisher',  to: 'publishers#show'
+    get  'public_key', to: 'services#public_key'
+    get  'test',       to: 'users#test'
+    post 'login',      to: 'users#login'
+    post 'register',   to: 'users#create'
+    get  'user',       to: 'users#show'
+    post 'notify',     to: 'users#send_notification'
+    get  'player',     to: 'players#my_player'
+    get  'publisher',  to: 'publishers#show'
 
-  #   resources :users, :except => [:destroy, :show] do
-  #     collection do
-  #       get '/confirm/:code',   to: 'users#confirm'
-  #       get '/:id/resend_code', to: 'users#resend_code'
-  #       get '/:wallet_address', to: 'users#find_player'
-  #     end
-  #   end
+    resources :users, :except => [:destroy, :show] do
+      collection do
+        get '/confirm/:code',   to: 'users#confirm'
+        get '/:id/resend_code', to: 'users#resend_code'
+        get '/:wallet_address', to: 'users#find_player'
+      end
+    end
 
-  #   resources :publishers, :except => [:show, :update, :destroy, :index] do
-  #     collection do
-  #       get   '/:user_id', to: 'publishers#show'
-  #       patch '/:user_id', to: 'publishers#update'
-  #       put   '/:user_id', to: 'publishers#update'
-  #     end
-  #   end
+    resources :publishers, :except => [:show, :update, :destroy, :index] do
+      collection do
+        get   '/:user_id', to: 'publishers#show'
+        patch '/:user_id', to: 'publishers#update'
+        put   '/:user_id', to: 'publishers#update'
+      end
+    end
 
-  #   resources :players, :except => [:show, :destroy] do
-  #     collection do
-  #       get   '/:user_id', to: 'players#show'
-  #       patch '/:user_id', to: 'players#update'
-  #       put   '/:user_id', to: 'players#update'
-  #     end
-  #   end
+    resources :players, :except => [:show, :destroy] do
+      collection do
+        get   '/:user_id', to: 'players#show'
+        patch '/:user_id', to: 'players#update'
+        put   '/:user_id', to: 'players#update'
+      end
+    end
 
-  #   resources :wallets, :except => [:show] do
-  #     collection do
-  #       get '/:wallet_address', to: 'wallets#show'
-  #       get '/:wallet_address/balance', to: 'wallets#balance'
-  #     end
-  #   end
-  # end
-  # root :to => 'home#index'
+    resources :wallets, :except => [:show] do
+      collection do
+        get '/:wallet_address', to: 'wallets#show'
+        get '/:wallet_address/balance', to: 'wallets#balance'
+      end
+    end
+  end
+  root :to => 'home#index'
 end
