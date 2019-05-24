99142bfb1fd8d8cd941b42434bc67ab1e677e23c
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-05-24 19:15:48 +0800

    Skips before authenticate on users#update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < Api::V1::BaseController
-  skip_before_action :authenticate_request, only: %i[create login confirm forgot]
+  skip_before_action :authenticate_request, only: %i[create login confirm forgot update]
   # before_action :doorkeeper_authorize!, except: %i[create login confirm update show forgot]
   skip_before_action :doorkeeper_authorize!
   before_action :check_current_user, only: %i[update update_password]
