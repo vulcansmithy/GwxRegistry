66f5d7b5b3f37eea31da0d941f553c8ffbe03849
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-05-30 12:49:34 +0800

    Refactor and fix rspec test.

--- spec/requests/api/v1/services_controller_spec.rb
+++ spec/requests/api/v1/services_controller_spec.rb
@@ -2,20 +2,22 @@ require "rails_helper"
 
 describe Api::V1::ServicesController do
   
-  # it "should be able to return Registry API public_key" do
+  it "should be able to return Registry API public_key" do
     
-  #   # call the API endpoint
-  #   get "/public_key"
+    # call the API endpoint
+    get "/public_key"
     
-  #   # retrieve result
-  #   result = JSON.parse(response.body)
+    # make sure the response was :ok
+    expect(response).to have_http_status(:ok)
     
-  #   # decode into raw form
-  #   retrieved_pk = Base64.decode64(result["public_key"])
-  #   saved_pk     = Base64.decode64(Rails.application.secrets.registry_public_key)
+    # retrieve result
+    result = JSON.parse(response.body)
+    
+    # decode into raw form
+    retrieved_public_key = result["public_key"]
 
-  #   # make sure the returned public_key matches
-  #   expect(retrieved_pk).to eq saved_pk
-  # end
+    # make sure the returned public_key matches
+    expect(retrieved_public_key).to eq PublicKeyEncryptionService.new.registry_public_key
+  end
   
 end
