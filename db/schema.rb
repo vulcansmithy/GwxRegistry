c11fc51d3aaa9c0ac0784344c2a434f1744d4c2c
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-06-06 17:10:53 +0800

    Implements transfer Cashier API wrapper

--- db/schema.rb
+++ db/schema.rb
@@ -47,8 +47,8 @@ ActiveRecord::Schema.define(version: 2019_06_03_021845) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "game_application_id"
-    t.text "platforms", default: [], array: true
     t.json "images"
+    t.text "platforms", default: [], array: true
     t.string "icon"
     t.string "url"
     t.index ["game_application_id"], name: "index_games_on_game_application_id"
