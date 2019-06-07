a0a89c1b3f3a161c20cc8b53e9c16149e7bddde1
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-06-07 12:16:08 +0800

    Adds transaction_details attributes in TriggerSerializer

--- app/serializers/trigger_serializer.rb
+++ app/serializers/trigger_serializer.rb
@@ -7,6 +7,10 @@ class TriggerSerializer < ActiveModel::Serializer
              :action_id,
              :player_profile_id,
              :created_at
+  
+  attribute :transaction_details do |trigger|
+    Cashier.new.find_transaction(trigger.transaction_id)
+  end
 
   belongs_to :action
   belongs_to :player_profile
