325cb82333f2556ad01e64a37ff5787473e87a11
Jestoni Talan <jestonivtalan@gmail.com>
2019-06-17 13:58:25 +0800

    Fixes conflict in player profile model

--- app/models/player_profile.rb
+++ app/models/player_profile.rb
@@ -1,4 +1,6 @@
 class PlayerProfile < ApplicationRecord
+  include Walletable
+
   after_commit :create_account, on: :create
   belongs_to   :user, optional: true
   belongs_to   :game
@@ -8,15 +10,4 @@ class PlayerProfile < ApplicationRecord
   validates_uniqueness_of :username
   validates_presence_of :username
   validates_format_of :username, with: /^[A-Za-z0-9_\.]+$/, multiline: true
-
-  private
-
-  def create_account
-    account = NemService.create_account
-
-    self.create_wallet(
-      wallet_address: account[:address],
-      pk: account[:priv_key]
-    )
-  end
 end
