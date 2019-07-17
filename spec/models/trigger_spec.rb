900c1ca42d381656dff2c3d468fcfc083cd1827c
Ly Escorial <lyescorial@gmail.com>
2019-07-17 19:20:28 +0800

    Resolves model spec error

--- spec/models/trigger_spec.rb
+++ spec/models/trigger_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Trigger, type: :model do
   it { should belong_to(:player_profile) }
-  it { should belong_to(:action) }
+  it { should belong_to(:action).optional }
 end
