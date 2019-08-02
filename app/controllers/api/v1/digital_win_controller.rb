5075adcf2d9aa2275537ed3fb5c647a586a9fd2c
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-08-02 11:21:37 +0800

    Adds DigitalWinController

---
+++ app/controllers/api/v1/digital_win_controller.rb
@@ -0,0 +1,17 @@
+class Api::V1::DigitalWinController < Api::V1::BaseController
+
+  def game_token
+    response = DigitalWinClient.get_game_token(game_token_params)
+    if response['GameResponse']['RespMessage'] == 'Success'
+      render json: response, status: :ok
+    else
+      render json: response, status: :bad_request
+    end
+  end
+
+  private
+
+  def game_token_params
+    params.permit(:game_id, :username)
+  end
+end
