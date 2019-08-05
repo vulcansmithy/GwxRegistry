a8a8ef92b6d1ef61da8c44524767af523ec5fe7e
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-08-05 10:21:53 +0800

    Adds seamless xml balance via username

--- app/controllers/api/v1/transfers_controller.rb
+++ app/controllers/api/v1/transfers_controller.rb
@@ -19,13 +19,21 @@ class Api::V1::TransfersController < Api::V1::BaseController
 
     source_wlt = params[:type] == 'debit' ? @user_wallet_address : @game_wallet_address
     destination_wlt = params[:type] == 'debit' ? @game_wallet_address : @user_wallet_address
+    balance = NemService.check_balance(source_wlt)
 
     response = CashierService.new.create_transaction(
       source_wallet: source_wlt,
       destination_wallet: destination_wlt,
       quantity: seamless_params[:quantity]
     )
-    success_response response
+
+    success_response(transaction: response, balance: balance)
+  end
+
+  def balance
+    player = PlayerProfile.find_by! username: params[:username]
+    balance = NemService.check_balance(player.user.wallet.wallet_address)
+    success_response(balance)
   end
 
   private
@@ -33,7 +41,7 @@ class Api::V1::TransfersController < Api::V1::BaseController
   def seamless_params
     params.permit(:game_id, :username, :quantity, :type)
   end
-  
+
   def transfer_params
     params.permit(
       :source_user_id,
