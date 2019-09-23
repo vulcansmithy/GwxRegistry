0cd3288e6b552e9f2ffc48f3263bd78fb8daf7e9
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-09-23 16:28:00 +0800

    Add featured field to games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_074700) do
+ActiveRecord::Schema.define(version: 2019_09_23_074123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_09_17_074700) do
     t.string "icon"
     t.string "url"
     t.string "cover"
+    t.boolean "featured", default: false
     t.index ["game_application_id"], name: "index_games_on_game_application_id"
     t.index ["publisher_id"], name: "index_games_on_publisher_id"
   end
