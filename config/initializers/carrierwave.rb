c89b5e6dd2ba5ac06a7f8b1e4af59cf4ee3a2bdc
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-10-02 12:50:09 +0800

    Use env variables all through out the repo

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,11 +3,11 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'ap-southeast-1'
     }
-    config.fog_directory = Rails.application.secrets.s3_bucket_name
+    config.fog_directory = ENV['S3_BUCKET_NAME']
     config.storage = :fog
   else
     config.storage = :file
