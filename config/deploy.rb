26321b0ea54aa72bd6f2a84846ef49fd34b5bbb1
Michael Rasheed Tan <mikkoyunltd@gmail.com>
2019-10-02 22:07:04 +0800

    Update ruby version in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock "~> 3.11.0"
 
 set :rbenv_type, :user
-set :rbenv_ruby, "2.5.1"
+set :rbenv_ruby, "2.5.7"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 set :rbenv_roles, :all
