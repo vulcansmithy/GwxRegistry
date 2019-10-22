1fece00d4d9a5d067ad37c32d08c65fa16fd940b
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-10-22 22:47:58 +0800

    Fix previously commented test.

--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe Publisher, type: :model do
-# @TODO the code below causing the test to fail. Need further investigating
-# it { should validate_uniqueness_of (:publisher_name) }
+  it { should validate_uniqueness_of (:publisher_name) }
   it { should validate_presence_of (:publisher_name) }
 end
